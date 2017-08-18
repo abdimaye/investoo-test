eaf327e5387729d458757b571a5df2f28366b411
abdimaye <abdul8002@gmail.com>
2017-08-18 01:21:11 +0100

    Revert "small changes"
    This reverts commit d5677609c63e155dc66ffa76bd2ab90a617fcd2f.

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\File;
-use App\Investoo\CsvFile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Investoo\CsvFile;
 
 class FileController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @param  \App\File  $file
      * @return \Illuminate\Http\Response
      */
@@ -67,7 +66,6 @@ class FileController extends Controller
     public function download(File $file, $id)
     {       
         if ($file = $file->find($id)) {
-            
             $filename = "{$file->id}.csv";
 
             if (Storage::disk('local')->exists($filename)) {
